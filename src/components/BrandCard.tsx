30345bfa1a2aa9d8ec647b8d464566f00eb0d208
신혜지 <liexil97@gmail.com>
2025-06-21 14:43:24 +0900

    브랜드

--- src/components/BrandCard.tsx
+++ src/components/BrandCard.tsx
@@ -4,13 +4,17 @@ import styled from 'styled-components';
 const BrandCardContainer = styled.div`
 width: 100%;
 height: 100%;
+
+padding: 40px 0;
+
 `;
 
 const BrandLayout = styled.div`
   display: flex;
   align-items: center;
-  gap: 24px;
-  padding: 20px;
+  gap: 50px;
+  padding-bottom: 50px;
+
   
   @media (min-width: 768px) {
     max-width: 60%;
@@ -27,19 +31,39 @@ const BrandImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+
 `;
 
 const ContentContainer = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
-  gap: 12px;
+  gap: 24px;
 `;
 
 const Title = styled.h2`
   font-size: ${({ theme }) => theme.fontSizes.lg};
   font-weight: ${({ theme }) => theme.fontWeights.bold};
   margin: 0;
+
+  h2 {
+    margin-left:8px;
+    display: inline-block;
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+    font-weight: ${({ theme }) => theme.fontWeights.bold};
+    position: relative;
+    z-index: 1;
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 60%;
+      background-color: ${({ theme }) => `${theme.colors.primary}80`};
+      z-index: -1;
+    }
+  }
 `;
 
 const Description = styled.p`
@@ -108,32 +132,29 @@ export const BrandCard = () => {
 
             <BrandLayout>
                 <ImageContainer>
-                    <BrandImage src="/path/to/your/image.jpg" alt="Brand" />
+                    <BrandImage src="/src/assets/doc.jpg" alt="Brand" />
                 </ImageContainer>
                 <ContentContainer>
-                    <Title>스위스에서 시작된 탈모 재생의 표준</Title>
+                    <Title>이보영 대표원장</Title>
                     <Description>
-                        모낭플란트는 오늘날 스위스를 대표하는 
-                        <h3>
-                        전문 탈모 재생 기술
-                        </h3>
-                        로 자리매김하고 있습니다. 
-                        <br />
-                        <br />
-                        스위스에서는 탈모 전문 클리닉에 방문하면 
-                        <h3>
-                        최소 한 명 이상의 모낭플란트 전문가
-                        </h3>
-                        를 만날 수 있을 만큼 보편화된 직업군이며, 
-                        이는 해당 분야의 기술력, 안전성, 효과성이 이미 국가적으로 검증되었음을 보여줍니다.
-                        <br />
-                        <br />
-                        특히 스위스는 모낭 건강과 두피 재생 분야의 기술을 꾸준히 발전시키며, 
-                        현재는 
-                        <h3>
-                        전 세계 탈모 재생 산업의 선두주자
-                        </h3>
-                        로서 확고한 위치를 차지하고 있습니다.
+                    모리셋을 시작했을 당시 한국은 두피 관리 불모지였습니다. 
+                    <br />
+                    <br />
+                    암 환자들이 사용할 수 있는 안전한 제품만을 고집 해왔던 모리셋은 
+                    엄격한 심사 과정 끝에 고품격 안전 국가인 덴마크에 본사를 둔 메조스킨라인을 선택하였습니다. 
+                    <br />
+                    <br />
+                    모리셋의 집념이 메조스킨라인을 만나 아시아 총판을 통해 프리미엄 제품과 특허 기술을 국내에 런칭하였으며, 탈모 불모지였던 한국에 오아시스와 같은 브랜드가 되었습니다. 
+                    <br />
+                    <br />
+
+                      · 모낭플란트 개발자 및 기술 보유자
+                      <br />
+                      · 스위스컬러 아시아 총판
+                      <br />
+                      · 메조스킨라인 아시아 총판
+                      <br />
+                      · 국내 최초 스위스컬러 국제강사
                     </Description>
                 </ContentContainer>
             </BrandLayout>
@@ -141,95 +162,50 @@ export const BrandCard = () => {
 
             <BrandLayout>
                 <ImageContainer>
-                    <BrandImage src="/path/to/your/image.jpg" alt="Brand" />
+                    <BrandImage src="/src/assets/lab1.jpg" alt="Brand" />
                 </ImageContainer>
                 <ContentContainer>
-                    <Title>한국, 새로운 모낭플란트 인재 양성의 시작점</Title>
+                    <Title>오직, 
+                      <h2>
+                      모리셋 모낭플란트
+                      </h2>
+                      </Title>
                     <Description>
-                    최근 대한민국에서도 탈모 인구의 증가와 함께, 탈모 개선의 중요성에 대한 사회적 인식이 빠르게 높아지고 있습니다. 
+                    
+                    모리셋은 25년간 누적된 메조스킨라인의 연구와 두피 개선 단계를 기반으로 '모낭플란트'를 개발하였으며, 
                     <br />
-                    단순 미용 목적을 넘어 
-                    <h3>
-                    건강 관리와 삶의 질 향상을 위한 필수 분야
-                    </h3>
-                    로 자리잡고 있는 탈모 케어.  
-                    이에 따라 전문적인 교육과 실습을 갖춘 모낭플란트 전문가의 양성이 더욱 절실한 시점입니다.
+                    필요한 모든 관리기간 및 단계 또한 과학적으로 설계되었습니다. 
                     <br />
                     <br />
-                    모리셋은 그 중심에서 
-                    <h3>
-                    모낭플란트 교육과 시스템 보급을 선도
-                    </h3>하고 있습니다. 
-                    이론 교육뿐 아니라 실무 중심의 커리큘럼을 바탕으로, 
-                    현장에서 바로 적용 가능한 실질적인 전문 인력을 양성하여, 국내 두피·탈모 시장에 질적인 변화를 이끌고 있습니다.   
+                    모리셋의 혁신적 기술력과 메조스킨라인의 제품이 만나 탄생한 모낭플란트는 두피 계의 완전한 솔루션임을 자부합니다. 
                     </Description>
                 </ContentContainer>
             </BrandLayout>
 
             <BrandLayout>
                 <ImageContainer>
-                    <BrandImage src="/path/to/your/image.jpg" alt="Brand" />
+                    <BrandImage src="/src/assets/cell.jpg" alt="Brand" />
                 </ImageContainer>
                 <ContentContainer>
-                    <Title>전 세대를 위한 두피 전문 프로그램</Title>
+                    <Title>모리셋 특허기술의 핵심,
+                      <h2>
+                    쏘팔메토 줄기세포
+                      </h2>
+                    </Title>
                     <Description>
-                        모리셋의 모낭플란트는 
-                        <h3>
-                        학생부터 노년층까지, 
-                        </h3>
-                        두피 건강이 필요한 모든 이들을 위한 프로그램입니다. 
-                        <br />
-                        각기 다른 두피 상태, 연령, 생활습관에 맞춰 개인별 맞춤 솔루션을 제공하며, 
-                        단순 관리가 아닌 
-                        <h3>
-                        과학적이고 체계적인 두피 재생 프로그램
-                        </h3>
-                        으로 발전하고 있습니다.
-
-                        <br />
-                        <br />
-
-                        우리의 목표는, 단지 머리카락을 되살리는 것을 넘어, 
-                        <h3>
-                        자신감을 회복하고 건강한 삶을 회복하는 것
-                        </h3>
-                        입니다.
+                    모리셋의 핵심성분은 쏘팔메토 줄기세포입니다. 
+                    <br />    
+                    <br />
+                    대서양 해안에서 자생하는 쏘팔메토는 탈모에 효과적인 식물성 줄기세포입니다. 모리셋은 유럽과 미국에서 보편화되어 있는 방식 그대로 쏘팔메토를 농축하여 국소 부위에 투여해 탈모의 원인이 되는 DHT 호르몬을 표적관리 및 생성억제합니다.    
+                    <br />
+                    <br />
+                    모리셋의 모낭플란트는 두피 건강을 위한 완전한 솔루션입니다. 
+                    <br />
+                    <br />
+                    모리셋의 모낭플란트는 두피 건강을 위한 완전한 솔루션입니다. 
                     </Description>
                 </ContentContainer>
             </BrandLayout>
-
-            <BrandLayout>
-                <ImageContainer>
-                    <BrandImage src="/path/to/your/image.jpg" alt="Brand" />
-                </ImageContainer>
-                <ContentContainer>
-                    <Title>탈모는 질병입니다, 모낭플란트는 해결책입니다</Title>
-                    <Description>
-                        현대 사회에서 탈모는 이제 단순한 외모 고민을 넘어, 
-                        <h3>
-                        정신적 스트레스와 사회적 위축을 유발하는 심각한 질환
-                        </h3>
-                        으로 다뤄지고 있습니다. 
-                        그렇기 때문에, 
-                        안전하고 효과적인 탈모 솔루션의 필요성
-                        은 더욱 강조되고 있습니다.
-
-                        <br />
-
-                        모리셋의 모낭플란트 시스템은 모든 사람에게 
-                        <h3>
-                        아름답고 건강한 두피를 회복시켜주는 해결책
-                        </h3>
-                        이자, 
-                        <h3>
-                        탈모를 질병으로 인식하는 현대 사회에 반드시 필요한 대안
-                        </h3>
-                        입니다.
-                        우리는 탈모로부터 자유로운 삶을 꿈꾸는 모든 이들에게, 신뢰와 과학, 그리고 확실한 결과를 약속드립니다.
-                    </Description>
-                </ContentContainer>
-            </BrandLayout>
-
 
             <DescText>
                 <h1>
