fa561bb71a5fd8fe06b968561883b11e7395bbbb
신혜지 <liexil97@gmail.com>
2025-06-22 13:06:12 +0900

    gitignores 추가

--- src/components/ProgramCard.tsx
+++ src/components/ProgramCard.tsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { MainCarousel } from './MainCarousel';
 
--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
